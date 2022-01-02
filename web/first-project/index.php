5dd16ebdcaaf8d159a4bbdbeda0a1362e6ba5a0d
anhduoc0935 <91368497+anhduoc0935@users.noreply.github.com>
2022-01-02 07:16:35 +0700

    Update index.php

--- web/first-project/index.php
+++ web/first-project/index.php
@@ -2,7 +2,7 @@
 define('BASE_PATH', __DIR__);
 define('BASE_APP', BASE_PATH.'/mvc');
 define('UPLOAD_DIR', BASE_PATH.'/public/uploads/');
-define('BASE_URL', 'http://chien-enpii.com/first-project');
+define('BASE_URL', '/first-project');
 
 require_once BASE_APP."/Middleware.php";
 session_set_cookie_params(0, "/"); 
